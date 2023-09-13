4f6ca69d62165f89349436df6059ebb7a6cb7910
Simone Arcari <53971242+simone-arcari@users.noreply.github.com>
2023-09-13 11:00:57 +0200

    go

--- gobackn.c
+++ gobackn.c
@@ -12,14 +12,13 @@
 
 // Struttura per passagio dati ai threads
 typedef struct {
-    int fd;
+    int socket;
     const void *buf; 
     size_t msg_size;
     int flags;
     struct sockaddr *addr; 
     socklen_t addr_len;
 } Thread_data;
-
 
 
 // Struttura per i pacchetti
@@ -42,6 +41,8 @@ typedef struct {
 Packet sender_buffer[WINDOW_SIZE];
 int sender_base = 0;
 int next_sequence_number = 0;
+int last_packet_acked = -1;
+int last_packet = -2;
 
 
 // Mutex per la sincronizzazione tra i thread
@@ -85,58 +86,101 @@ bool verify_ack_checksum(Ack ack) {
 void *send_packets(void *arg) {
     Packet packet;
     Thread_data *args = (Thread_data*)arg;
+    int socket = args->socket;
+    struct sockaddr *addr = args->addr; 
+    socklen_t addr_len = args->addr_len;
+
+    size_t data_size;                           // dimensione in byte dei dati effettivamente trasmessi dal pachhetto corrente 
+    size_t msg_size = args->msg_size;           // dimensione in byte del messaggio
     void *buffer = args->buf;                   // contine il messaggio del livello applicativo da dividere in pacchetti
-    void *buffer_ptr = buffer;                  // ad ogni iterazione puntare l'inizio della zona di memoria per formare il pacchetto corrente
+    void *buffer_ptr = buffer;                  // ad ogni iterazione punta l'inizio della zona di memoria per formare il pacchetto corrente
 
-    
-    size_t number_of_packets = args->msg_size/DATA_SIZE;
+    int last_packet;
+    int number_of_full_packets = msg_size/DATA_SIZE;      // calcola il numero di pacchetti che avranno taglia massima
+    size_t residue_packet_size = msg_size%DATA_SIZE;      // calcola la taglia del pachetto residuo che tipicamente è minore di DATA_SIZE
 
+
+    if (residue_packet_size == 0) {
+        last_packet = number_of_full_packets-1;  // si conta da zero
+
+    } else {
+        last_packet = number_of_full_packets;   // si conta da zero
+
+    }
 
 
     while (true) {
         // Controlla se c'è spazio nel buffer di trasmissione
         if (next_sequence_number < sender_base + WINDOW_SIZE) {
 
-            
+            if (next_sequence_number == number_of_full_packets) {
+                data_size = residue_packet_size;
 
+            } else {
+                data_size = DATA_SIZE;
 
+            }
+
+            // crea il pacchetto
             packet.sequence_number = next_sequence_number;
-            memcpy(packet.data, buffer_ptr, ); 
-            packet.checksum = calculate_checksum(packet); // Calcola la checksum
+            memcpy(packet.data, buffer_ptr, data_size); 
+            packet.checksum = calculate_checksum(packet);      // Calcola la checksum
 
             // Aggiungi il pacchetto al buffer di trasmissione
             sender_buffer[next_sequence_number % WINDOW_SIZE] = packet;
 
             // Invia il pacchetto
-            sendto(args->fd, &packet, sizeof(packet), 0, args->addr, args->addr_len);
+            sendto(socket, &packet, sizeof(packet), 0, addr, addr_len);
 
             // Avanza il numero di sequenza e il puntatore al buffer
             next_sequence_number++;
-            buffer_ptr += sizeof(packet.data)*next_sequence_number;
+            buffer_ptr += sizeof(packet.data);      // tutti i calcoli sono riferiti all'unità intesa in byte
 
         }
+
+
+        if (last_packet_acked == last_packet) {
+            return NULL;
+
+        }
+
     }
-    return NULL;
 }
 
 // Funzione per ricevere un ACK
 void *receive_acks(void *arg) {
     Ack received_ack;
     Thread_data *args = (Thread_data*)arg;
+    int socket = args->socket;
+    struct sockaddr *addr = args->addr; 
+    socklen_t addr_len = args->addr_len;
+
  
     while (true) {
-        recvfrom(args->fd, &received_ack, sizeof(received_ack), 0, args->addr, args->addr_len);
+        recvfrom(args->socket, &received_ack, sizeof(received_ack), 0, addr, addr_len);
 
         // Verifica la checksum dell'ACK ricevuto
         if (verify_ack_checksum(received_ack)) {
 
             // ACK ricevuto correttamente, aggiornare il base sender
             if (received_ack.sequence_number >= sender_base) {
+
+                last_packet_acked = received_ack.sequence_number;
+
+                if (last_packet_acked )
                 sender_base = received_ack.sequence_number + 1;
+                
             }
 
         } else {
-            // La checksum dell'ACK non corrisponde, ignora l'ACK o gestisci l'errore
+            // La checksum dell'ACK non corrisponde, ignora l'ACK
+
+        }
+
+
+        if (last_packet_acked == last_packet) {
+            return NULL;
+
         }
 
     }
@@ -145,9 +189,9 @@ void *receive_acks(void *arg) {
 
 
 
-void send_msg(int fd, const void *buf, size_t n, int flags, struct sockaddr *addr, socklen_t addr_len) {
+void send_msg(int socket, const void *buf, size_t n, int flags, struct sockaddr *addr, socklen_t addr_len) {
     pthread_t send_thread, receive_thread;
-    Thread_data args = {fd, buf, n , flags, addr, addr_len};
+    Thread_data args = {socket, buf, n , flags, addr, addr_len};
 
 
 
